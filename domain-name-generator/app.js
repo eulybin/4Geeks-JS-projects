347757a2c364c7f4b070870c478eb7578ba5b0db
Egor Ulybin <egorulybin@gmail.com>
2025-07-18 20:18:06 +0200

    added starting dataset to app.js

--- domain-name-generator/app.js
+++ domain-name-generator/app.js
@@ -1,10 +1,8 @@
-import 'bootstrap';
-import './style.css';
-
-import './assets/img/rigo-baby.jpg';
-import './assets/img/4geeks.ico';
-
 window.onload = function () {
-  //write your code here
-  console.log('Hello Rigo from the console!');
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extensions = ['.com', '.org', '.net'];
 };
+
+// thegreatjogger.com
