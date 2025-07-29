17af41991b5707ddc9a07905c377cc147c6c2655
Egor Ulybin <egorulybin@gmail.com>
2025-07-29 17:30:26 +0200

    added the randomCardNumber span to .card div using appendChild()

--- random-card-proj/app.js
+++ random-card-proj/app.js
@@ -1,21 +1,14 @@
 window.onload = function () {
   const suits = ['spade', 'heart', 'club', 'diamond'];
-  const cardNumbers = [
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '10',
-    'J',
-    'Q',
-    'K',
-    'A',
-  ];
+  const cardNumbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
-  const randomSuitNumber = Math.floor(Math.random() * 4);
-  const randomCardNumber = Math.floor(Math.random() * 13);
+  const randomSuitNumber = suits[Math.floor(Math.random() * suits.length)];
+  const randomCardNumber = cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
+
+  // Create the <span> tag where the card number will go inside the .card <div>
+  const mainDiv = document.querySelector('.card');
+  const printRandomCardNumber = document.createElement('span');
+  printRandomCardNumber.textContent = randomCardNumber;
+  printRandomCardNumber.style.fontSize = '128px';
+  mainDiv.appendChild(printRandomCardNumber);
 };
