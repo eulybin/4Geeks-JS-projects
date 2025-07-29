c35e24a6676ec85fbe0dc9044a2a747951b3cd2b
Egor Ulybin <egorulybin@gmail.com>
2025-07-29 17:05:21 +0200

    created the suits and cardNumbers arrays with their corresponding random number variables

---
+++ random-card-proj/app.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+  const suits = ['spade', 'heart', 'club', 'diamond'];
+  const cardNumbers = [
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K',
+    'A',
+  ];
+
+  const randomSuitNumber = Math.floor(Math.random() * 4);
+  const randomCardNumber = Math.floor(Math.random() * 13);
+};
