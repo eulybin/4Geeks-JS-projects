d7f5aaf65ce47eb12d0f275e654d16a08d127758
Egor Ulybin <egorulybin@gmail.com>
2025-07-31 17:07:45 +0200

    created the switch statement to generate the card suit and display it

--- random-card-proj/app.js
+++ random-card-proj/app.js
@@ -2,13 +2,36 @@ window.onload = function () {
   const suits = ['spade', 'heart', 'club', 'diamond'];
   const cardNumbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
-  const randomSuitNumber = suits[Math.floor(Math.random() * suits.length)];
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
   const randomCardNumber = cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
 
-  // Create the <span> tag where the card number will go inside the .card <div>
+  // Generate and append the card number
   const mainDiv = document.querySelector('.card');
   const printRandomCardNumber = document.createElement('span');
   printRandomCardNumber.textContent = randomCardNumber;
   printRandomCardNumber.style.fontSize = '158px';
   mainDiv.appendChild(printRandomCardNumber);
+
+  // Generate suit and display it
+  const heartDiv = document.querySelector('.heart');
+  const diamondDiv = document.querySelector('.diamond');
+  const spadeDiv = document.querySelector('.spade');
+  const clubDiv = document.querySelector('.club');
+  switch (randomSuit) {
+    case 'heart':
+      heartDiv.style.display = 'block';
+      break;
+    case 'diamond':
+      diamondDiv.style.display = 'block';
+      break;
+    case 'spade':
+      spadeDiv.style.display = 'block';
+      break;
+    case 'club':
+      clubDiv.style.display = 'block';
+      break;
+    default:
+      clubDiv.style.display = 'block';
+      break;
+  }
 };
